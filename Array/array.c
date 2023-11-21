5fc40ab0d7481c0e28991ec58f8a0871b149b5de
Gabriel Amaral <gcbsamaral.dev@gmail.com>
2023-11-21 11:59:25 -0300

    Adding array code

---
+++ Array/array.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+//Handle operations
+void swap(int array[], int index_a, int index_b){
+    int temp = array[index_a];
+    array[index_a] = array[index_b];
+    array[index_b] = temp;
+
+    return;
+}
+
+void fill(int array[], int size) {
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+        array[i] = rand() % 1000;
+  
+    return;
+}
+
+void shuffle(int array[], int size){
+    srand(time(NULL));
+    for(int i = 0; i < size; i++)
+        swap(array, i, rand() % size)
+  
+    return;
+}
+
+//Fetch operations
+int binary_search(int array[], int high, int key){
+    int low = 0; 
+    
+    do{
+        int mid = (high+low)/2;
+        
+        if(key < array[mid])
+            high = mid-1;
+        else if (key > array[mid])
+            low = mid+1;
+        else
+            return mid;
+        
+    }while(low <= high);
+
+    return -1;
+}
+
+int recursive_binary_search(int array[], int low, int high, int key){
+    if(low > high)
+        return -1;
+
+    int mid = (high+low)/2;
+    if(key < array[mid])
+        return recursive_binary_search(array, low, mid-1, key);
+    else if(key > array[mid])
+        return recursive_binary_search(array, mid+1, high, key);
+    else
+        return mid;
+}
+
+//View operations
+void print(int array[], int size){
+    for(int i = 0; i < size - 1; i++)
+        printf("[%s]  ", array[i]);
+    printf("[%s]\n", array[size-1]);
+
+    return;
+}
+
+
---
+++ Array/array.h
@@ -0,0 +1,16 @@
+#ifndef ARRAY_H_INCLUDED
+#define ARRAY_H_INCLUDED
+
+//View operations
+void swap(int array[], int index_a, int index_b);
+void fill(int array[], int size);
+void shuffle(int array[], int size);
+
+//Fetch operations
+int binary_search(int array[], int high, int key);
+int recursive_binary_search(int array[], int low, int high, int key);
+
+//View operations
+void print(int array[], int size);
+
+#endif
